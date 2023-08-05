9efb658df6dfe01ff88e4e4f0b2b209c9e0e8f28
lambert34 <aleksandrlambert@gmail.com>
2023-08-05 11:47:07 +0700

    доделал слайдер

--- js/main.js
+++ js/main.js
@@ -1,5 +1,10 @@
 const swiper = new Swiper(".swiper", {
+    effect: "fade",
     pagination: {
         el: ".swiper-pagination",
+    },
+    autoplay: {
+        delay: 3400,
+        disableOnInteraction: false,
     },
 });
