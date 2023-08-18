66997ed2fe436eaff68f8e9b0e535e7a8e0fc8b9
lambert34 <aleksandrlambert@gmail.com>
2023-08-18 16:31:49 +0700

    добавляет ряд динамических функций, создает новый раздел-каталог

--- js/main.js
+++ js/main.js
@@ -1,10 +1,28 @@
-const swiper = new Swiper(".swiper", {
-    effect: "fade",
-    pagination: {
-        el: ".swiper-pagination",
-    },
-    autoplay: {
-        delay: 3400,
-        disableOnInteraction: false,
-    },
+const tabItem = document.querySelectorAll('.tabs__btn-item');
+const tabContent = document.querySelectorAll('.tabs__content-item');
+
+tabItem.forEach(function(element) {
+    element.addEventListener('click', open);
 });
+
+    function open(evt) {
+        const tabTarget = evt.currentTarget;
+
+        tabItem.forEach(function(item) {
+            item.classList.remove('tabs__btn-item--active');
+        })
+
+        tabTarget.classList.add('tabs__btn-item--active');  
+        };
+
+
+        const swiper = new Swiper(".swiper", {
+            effect: "fade",
+            pagination: {
+                el: ".swiper-pagination",
+            },
+            autoplay: {
+                delay: 3400,
+                disableOnInteraction: false,
+            },
+        });
