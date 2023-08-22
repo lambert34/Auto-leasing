22b5334c3ac114177e41525e2bb267c2c2db176a
lambert34 <aleksandrlambert@gmail.com>
2023-08-22 16:18:17 +0700

    добавляет немного адаптива и бургер меню с кодом js

--- js/main.js
+++ js/main.js
@@ -26,3 +26,12 @@ tabItem.forEach(function(element) {
                 disableOnInteraction: false,
             },
         });
+
+        const menuBtn = document.querySelector('.menu__btn');
+        const menu = document.querySelector('.menu__list');
+
+        menuBtn.addEventListener('click', () => {
+            menu.classList.toggle('menu__list--active');
+        });
+
+        
